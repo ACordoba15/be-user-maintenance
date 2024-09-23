f721acbbd85dc6e3af3557a37f8909ef4d4cd853
Andrés Córdoba Romero <jancordoba05@gmail.com>
2024-09-23 14:03:03 -0600

    Corrección en las rutas

--- routes/record.ts
+++ routes/record.ts
@@ -4,9 +4,9 @@ import { DeleteRecord, GetRecord, GetRecords, PostRecord, PutRecord } from '../c
 const router = Router();
 // Se definen las rutas y se asocian con el controlador
 router.get('/all',    GetRecords);
-router.get('/id',     GetRecord);
+router.get('/:id',     GetRecord);
 router.post('/',      PostRecord);
-router.put('/id',     PutRecord);
-router.delete('/id',  DeleteRecord);
+router.put('/:id',     PutRecord);
+router.delete('/:id',  DeleteRecord);
 
 export default router;
--- routes/user.ts
+++ routes/user.ts
@@ -4,9 +4,9 @@ import { DeleteUser, GetUser, GetUsers, PostUser, PutUser } from '../controllers
 const router = Router();
 // Se definen las rutas y se asocian con el controlador
 router.get('/all',    GetUsers);
-router.get('/id',     GetUser);
+router.get('/:id',     GetUser);
 router.post('/',      PostUser);
-router.put('/id',     PutUser);
-router.delete('/id',  DeleteUser);
+router.put('/:id',     PutUser);
+router.delete('/:id',  DeleteUser);
 
 export default router;
